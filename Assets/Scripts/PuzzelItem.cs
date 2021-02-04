aa81fd2662d3912f5d455ac0e78e1ffce70de3a2
BoLadan <60391973+BoLadan@users.noreply.github.com>
2021-02-04 11:26:30 +0100

    PuzzelItem
    made the PuzzelItem class and tested it.
    passed all tests

---
+++ Assets/Scripts/PuzzelItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzelItem : Item
+{
+    private string riddle;
+    private string answer;
+
+    private bool solved;
+
+    public PuzzelItem(string name, float weight, string riddle, string answer) : base(name, weight)
+    {
+        this.riddle = riddle;
+        this.answer = answer;
+        this.solved = false;
+    }
+
+    public string GetRiddle()
+    {
+        return riddle;
+    }
+
+    public string GetAnswer()
+    {
+        return answer;
+    }
+
+    public bool GetSolved()
+    {
+        return solved;
+    }
+
+    public bool CheckForAnswer(string m_answer)
+    {
+        if (m_answer == answer)
+        {
+            solved = true;
+        }
+        else
+        {
+            solved = false;
+        }
+        return solved;
+    }
+}
