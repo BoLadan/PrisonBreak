4b49ed91aa908727ad80c80e764ea0296f04621b
BoLadan <60391973+BoLadan@users.noreply.github.com>
2021-02-04 15:05:07 +0100

    first person controller
    downlaoded and imported a first person controller and added it into the scene

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IInteractable
+{
+    
+
+    public void Action(PlayerManager player)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private Inventory inventory;
+
+    public float initialMaxWeight = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new Inventory(initialMaxWeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
